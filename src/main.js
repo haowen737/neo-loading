8da8d103ac2584c93ef15b6b51313c3c06245729
Haowen <zt452268020@live.com>
2017-11-20 18:26:49 +0800

    new start

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import neoLoadingTemplate from './component/NeoLoading.vue'
+let $vm
+export default {
+  install (Vue, options = {}) {
+    if (!$vm) {
+      let NeoLoadingTemplate = Vue.extend(neoLoadingTemplate)
+
+      let $vm = new NeoLoadingTemplate({
+        el: document.createElement('div')
+      })
+
+      let { backgroundColor, color, backgroundOpacity } = options
+      $vm.backgroundColor = backgroundColor || '#FFFFFF'
+      $vm.backgroundOpacity = backgroundOpacity || '0.5'
+      $vm.color = color || '#000'
+
+      document.body.appendChild($vm.$el)
+
+      let neoLoading = function () {}
+
+      neoLoading.show = () => { $vm.show = true }
+      neoLoading.hide = () => { $vm.show = false }
+
+      Vue.prototype.$neoLoading = neoLoading
+    }
+  }
+}
